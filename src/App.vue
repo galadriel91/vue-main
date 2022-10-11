fa6efe535591768ff22ec7d1df8c4afbad1df0fa
GalaDriel91 <g_ala@naver.com>
2022-10-11 10:09:46 +0900

    Update: Components - Common - MainLoading

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <MainFooter />
+        <MainLoading />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import MainFooter from './components/common/MainFooter.vue';
+import MainLoading from './components/common/MainLoading.vue';
 
 export default defineComponent({
     components: {
         MainFooter,
+        MainLoading,
     },
     setup() {
         return {};
