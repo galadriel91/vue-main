0b2b02b9f0b8565305fad66edbaad449b0279fb9
GalaDriel91 <g_ala@naver.com>
2022-10-11 13:46:39 +0900

    Add: Components - Contact - ContactModal

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -3,21 +3,34 @@
         <h1>CONTACT</h1>
         <ContactInfo />
         <ContactForm />
+        <ContactModal :isModal="isModal">
+            <template #alertMessage>{{ modalMessage }}</template>
+        </ContactModal>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useCommon } from '@/store/commonStore';
+import { storeToRefs } from 'pinia';
 import ContactInfo from '../components/contact/ContactInfo.vue';
 import ContactForm from '../components/contact/ContactForm.vue';
+import ContactModal from '../components/contact/ContactModal.vue';
 
 export default defineComponent({
     components: {
         ContactInfo,
         ContactForm,
+        ContactModal,
     },
     setup() {
-        return {};
+        const commonStore = useCommon();
+        const { isModal, modalMessage } = storeToRefs(commonStore);
+        // useLoading();
+        return {
+            isModal,
+            modalMessage,
+        };
     },
 });
 </script>
