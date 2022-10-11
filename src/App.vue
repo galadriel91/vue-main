839b2031746a63353cca7a0cfd380974d45401b6
GalaDriel91 <g_ala@naver.com>
2022-10-11 09:46:21 +0900

    Update: Components - Common - MainFooter

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-    <div>App</div>
+    <div>
+        <MainFooter />
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import MainFooter from './components/common/MainFooter.vue';
 
 export default defineComponent({
+    components: {
+        MainFooter,
+    },
     setup() {
         return {};
     },
