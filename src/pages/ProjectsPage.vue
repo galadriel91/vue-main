004c30aa5f208a2fce8c09012392b9fec7f76bc6
GalaDriel91 <g_ala@naver.com>
2022-10-11 14:24:51 +0900

    Add: Components - Projects - ProjectsList

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -2,15 +2,19 @@
     <div class="projects">
         <div class="projectsTitle">
             <h2>PROJECTS</h2>
+            <ProjectsList />
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ProjectsList from '../components/projects/ProjectsList.vue';
 
 export default defineComponent({
-    components: {},
+    components: {
+        ProjectsList,
+    },
     setup() {
         return {};
     },
