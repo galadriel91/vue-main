0edb04f16f56c74cfe3044a800825a47d673208b
GalaDriel91 <g_ala@naver.com>
2022-10-11 10:53:53 +0900

    Add: Components - Contact - ContactInfo

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="contact">
         <h1>CONTACT</h1>
+        <ContactInfo />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ContactInfo from '../components/contact/ContactInfo.vue';
 
 export default defineComponent({
+    components: {
+        ContactInfo,
+    },
     setup() {
         return {};
     },
