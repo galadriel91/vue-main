b12b6621cef82e48d46d76ec9e7c5cf178d5bb33
GalaDriel91 <g_ala@naver.com>
2022-10-11 12:04:41 +0900

    Add: Components - Contact - ContactForm

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -2,16 +2,19 @@
     <div class="contact">
         <h1>CONTACT</h1>
         <ContactInfo />
+        <ContactForm />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import ContactInfo from '../components/contact/ContactInfo.vue';
+import ContactForm from '../components/contact/ContactForm.vue';
 
 export default defineComponent({
     components: {
         ContactInfo,
+        ContactForm,
     },
     setup() {
         return {};
--- src/store/commonStore.ts
+++ src/store/commonStore.ts
@@ -3,6 +3,8 @@ import { defineStore } from 'pinia';
 export const useCommon = defineStore('common', {
     state: () => ({
         loading: false,
+        isModal: false,
+        modalMessage: '',
     }),
     actions: {
         ON_LOADING() {
@@ -10,6 +12,17 @@ export const useCommon = defineStore('common', {
         },
         OFF_LOADING() {
             this.loading = false;
+        },
+        MODAL_SUCCESS() {
+            this.isModal = true;
+            this.modalMessage = '성공적으로 메일을 보냈습니다.';
+        },
+        MODAL_FAIL() {
+            this.isModal = true;
+            this.modalMessage = '다시 한번 확인해 주세요';
+        },
+        CLOSE_MODAL() {
+            this.isModal = false;
         },
     },
 });
