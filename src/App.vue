c55ebdd9f6ea7adf7671633f245bd8546906bd81
GalaDriel <g_ala@naver.com>
2022-11-07 15:22:27 +0900

    Update: Apply Fullpage - vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
     <div>
         <MainHeader />
-        <div class="pageWrap">
-            <RouterView />
-        </div>
-        <MainFooter />
-        <MainLoading />
+        <FullPage ref="fullpage" :options="options" id="fullpage">
+            <div class="section">
+                <MainSlide />
+            </div>
+            <div class="section">
+                <MainAbout />
+            </div>
+            <div class="section">
+                <MainContact />
+            </div>
+        </FullPage>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import MainFooter from './components/common/MainFooter.vue';
-import MainLoading from './components/common/MainLoading.vue';
+import { defineComponent, reactive } from 'vue';
 import MainHeader from './components/common/MainHeader.vue';
+import MainAbout from './components/main/MainAbout.vue';
+import MainContact from './components/main/MainContact.vue';
+import MainSlide from './components/main/MainSlide.vue';
 
 export default defineComponent({
     components: {
-        MainFooter,
-        MainLoading,
+        MainAbout,
+        MainContact,
         MainHeader,
+        MainSlide,
     },
     setup() {
-        return {};
+        const options = reactive({
+            licenseKey: 'YOUR_KEY_HEERE',
+            menu: '#menu',
+            anchors: ['page1', 'page2', 'page3'],
+            sectionsColor: ['#fff', '#f7f7f7', '#0798ec'],
+        });
+        return {
+            options,
+        };
     },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+import 'vue-fullpage.js/dist/style.css';
+import VueFullPage from 'vue-fullpage.js';
+
 import { createApp } from 'vue';
 import { router } from '@/routes/index';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 const pinia = createPinia();
 
-createApp(App).use(pinia).use(router).mount('#app');
+createApp(App).use(VueFullPage).use(pinia).use(router).mount('#app');
