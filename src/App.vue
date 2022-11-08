2e052cc119546da7d421872336b3cc2a35fb86bc
GalaDriel <g_ala@naver.com>
2022-11-08 13:04:15 +0900

    <Vue Main Reinitial Commit>

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,47 @@
-<template>
-    <div>
-        <MainHeader />
-        <FullPage ref="fullpage" :options="options" id="fullpage">
-            <div class="section">
-                <MainSlide />
-                <MainIntro />
-                <!-- <MainSwiper /> -->
-            </div>
-            <div class="section">
-                <MainAbout />
-            </div>
-            <div class="section">
-                <MainProjects />
-            </div>
-            <div class="section">
-                <MainContact />
-            </div>
-        </FullPage>
-    </div>
-</template>
-
-<script lang="ts">
-import { defineComponent, reactive } from 'vue';
-import MainAbout from '@/components/about/MainAbout.vue';
-import MainHeader from '@/components/common/MainHeader.vue';
-import MainContact from '@/components/contact/MainContact.vue';
-import MainProjects from '@/components/projects/MainProjects.vue';
-import MainSlide from './components/main/MainSlide.vue';
-import MainIntro from './components/main/MainIntro.vue';
-// // import MainSwiper from './components/main/MainSwiper.vue';
-
-export default defineComponent({
-    components: {
-        MainIntro,
-        MainAbout,
-        MainContact,
-        MainHeader,
-        MainSlide,
-        MainProjects,
-        // MainSwiper,
-    },
-    setup() {
-        const options = reactive({
-            licenseKey: import.meta.env.VITE_APP_FULLPAGE_KEY,
-            menu: '#menu',
-            anchors: ['home', 'about', 'projects', 'contact'],
-            sectionsColor: ['#fff', '#f7f7f7', '#fff', '#fff'],
-        });
-        return {
-            options,
-        };
-    },
-});
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-<style lang="scss">
-@import '@/assets/scss/index.scss';
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
 </style>
