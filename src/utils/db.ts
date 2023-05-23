53ddad64f19b1f221b78a62290aa25522398ecd2
GalaDriel <g_ala@naver.com>
2023-05-23 16:17:25 +0900

    Simplify: image compress & add airbusan db

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -132,49 +132,6 @@ const db: ProjectItem[] = [
     //         ],
     //     },
     // },
-    // {
-    //     id: 6,
-    //     title: '에어부산',
-    //     vue: false,
-    //     markup: true,
-    //     posts: {
-    //         mainImage: '/images/air.png',
-    //         subtitle: [
-    //             '피그마와 HTML & CSS & JS를 사용하여',
-    //             '에어부산 클론사이트를 마크업 하였습니다.',
-    //         ],
-    //         info: [
-    //             {
-    //                 pc: '/images/busanDesk.png',
-    //                 alt: '데스크탑 정보',
-    //             },
-    //             {
-    //                 mobile: '/images/busanMobile.png',
-    //                 alt: '모바일 정보',
-    //             },
-    //         ],
-    //         sites: [
-    //             {
-    //                 link: 'https://lothlorien91.cafe24.com/airbusan/index.html',
-    //                 name: '클론 사이트',
-    //             },
-    //             {
-    //                 link: 'https://www.figma.com/file/qrrQeB2b7JCWGIjw88oFm5/AirBusan?node-id=1%3A2285&t=9dW3jwecITqwBb97-1',
-    //                 name: '피그마 상세정보',
-    //             },
-    //         ],
-    //         images: [
-    //             {
-    //                 pc: '/images/airPc.png',
-    //                 alt: '데스크탑 이미지',
-    //             },
-    //             {
-    //                 mobile: '/images/noteMo.png',
-    //                 alt: '모바일 이미지',
-    //             },
-    //         ],
-    //     },
-    // },
     {
         id: 3,
         title: '해커뉴스',
@@ -211,13 +168,13 @@ const db: ProjectItem[] = [
                     name: '클론 사이트',
                 },
                 {
-                    link: 'https://www.figma.com/file/5KmFKNmJrWTqBPMozrtyvS/Hacker-News?node-id=0%3A1&t=xRryeQKA9ZA0WLnE-1',
+                    link: 'https://www.figma.com/file/5KmFKNmJrWTqBPMozrtyvS/Hacker-News?type=design&node-id=0%3A1&t=WLpeBnprBbbGxoGy-1',
                     name: '피그마 상세정보',
                 },
             ],
             images: [
                 {
-                    pc: '/images/hackerPc.png',
+                    pc: '/images/hackerPc.jpg',
                     alt: '데스크탑 이미지',
                 },
             ],
@@ -259,13 +216,13 @@ const db: ProjectItem[] = [
                     name: '클론 사이트',
                 },
                 {
-                    link: 'https://www.figma.com/file/tkeir6ApbYs8pfqhT1lImF/Hollys-Coffee?node-id=1%3A1156&t=PHY3xpha24vrcUdI-1',
+                    link: 'https://www.figma.com/file/tkeir6ApbYs8pfqhT1lImF/Hollys-Coffee?type=design&node-id=1%3A1156&t=mZyLkxGMYDwriA5O-1',
                     name: '피그마 상세정보',
                 },
             ],
             images: [
                 {
-                    pc: '/images/hollysPc.png',
+                    pc: '/images/hollysPc.jpg',
                     alt: '데스크탑 이미지',
                 },
             ],
@@ -307,13 +264,61 @@ const db: ProjectItem[] = [
                     name: '클론 사이트',
                 },
                 {
-                    link: 'https://www.figma.com/file/njXxe2XZliaJZdBcbzYU0k/AlphaColor?node-id=2%3A1155&t=hRfK7RXT3L2FdnqT-1',
+                    link: 'https://www.figma.com/file/njXxe2XZliaJZdBcbzYU0k/AlphaColor?type=design&node-id=124%3A10&t=mNqAZ5q6nrykblBg-1',
                     name: '피그마 상세정보',
                 },
             ],
             images: [
                 {
-                    pc: '/images/alphaPc.png',
+                    pc: '/images/alphaPc.jpg',
+                    alt: '데스크탑 이미지',
+                },
+            ],
+        },
+    },
+    {
+        id: 6,
+        title: '에어부산',
+        vue: false,
+        markup: true,
+        posts: {
+            mainImage: '/images/air.png',
+            description: [
+                'HTML & CSS & JS를 사용하여 에어부산',
+                '메인 페이지를 퍼블리싱 하였습니다.',
+            ],
+            skill: ['html', 'css', 'js', 'scss'],
+            subInfo: [
+                {
+                    title: '반응형',
+                    content: 'X',
+                },
+                {
+                    title: '배포',
+                    content: '카페24',
+                },
+                {
+                    title: '라이브러리',
+                    content: 'Slick',
+                },
+                {
+                    title: '총페이지',
+                    content: '메인 (1)',
+                },
+            ],
+            sites: [
+                {
+                    link: 'https://lothlorien91.cafe24.com/airbusan/index.html',
+                    name: '클론 사이트',
+                },
+                {
+                    link: 'https://www.figma.com/file/qrrQeB2b7JCWGIjw88oFm5/AirBusan?type=design&node-id=1%3A2285&t=hbEAPZnklbbdlBPX-1',
+                    name: '피그마 상세정보',
+                },
+            ],
+            images: [
+                {
+                    pc: '/images/airPc.jpg',
                     alt: '데스크탑 이미지',
                 },
             ],
@@ -355,13 +360,13 @@ const db: ProjectItem[] = [
                     name: '클론 사이트',
                 },
                 {
-                    link: 'https://www.figma.com/file/1nHaSnBesorJAZGUKqHJjS/Domino?node-id=5%3A1155&t=ktWVoXog1LwBKbM6-1',
+                    link: 'https://www.figma.com/file/1nHaSnBesorJAZGUKqHJjS/Domino?type=design&node-id=5%3A1155&t=q9hvDrmVANrNOfVT-1',
                     name: '피그마 상세정보',
                 },
             ],
             images: [
                 {
-                    pc: '/images/dominoPc.png',
+                    pc: '/images/dominoPc.jpg',
                     alt: '데스크탑 이미지',
                 },
             ],
