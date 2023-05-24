a044f067e424f52e8adfe0e8f091afd03d8d1e01
GalaDriel <g_ala@naver.com>
2023-05-24 17:42:15 +0900

    Update: Add ShillaHotel data in db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -45,93 +45,103 @@ const db: ProjectItem[] = [
     //         ],
     //     },
     // },
-    // {
-    //     id: 2,
-    //     title: '포트폴리오',
-    //     vue: true,
-    //     markup: true,
-    //     posts: {
-    //         mainImage: '/images/logo.png',
-    //         subtitle: [
-    //             'JS의 프레임워크 중 하나인 Vue.js를 사용하여',
-    //             '포트폴리오 사이트를 제작 하였습니다.',
-    //         ],
-    //         info: [
-    //             {
-    //                 pc: '/images/portDesk.png',
-    //                 alt: '데스크탑 정보',
-    //             },
-    //             {
-    //                 mobile: '/images/portMobile.png',
-    //                 alt: '모바일 정보',
-    //             },
-    //         ],
-    //         sites: [
-    //             {
-    //                 link: 'https://whitetree.netlify.app',
-    //                 name: '클론 사이트',
-    //             },
-    //             {
-    //                 link: 'https://www.figma.com/file/oH7FG7EQBigCOKb9iUfaCj/Portfolio?node-id=1%3A1155&t=uuCKI9cPTFSTzbSK-1',
-    //                 name: '피그마 상세정보',
-    //             },
-    //         ],
-    //         images: [
-    //             {
-    //                 pc: '/images/portPc.jpg',
-    //                 alt: '데스크탑 이미지',
-    //             },
-    //             {
-    //                 mobile: '/images/noteMo.png',
-    //                 alt: '모바일 이미지',
-    //             },
-    //         ],
-    //     },
-    // },
-    // {
-    //     id: 5,
-    //     title: '신라호텔',
-    //     markup: true,
-    //     react: false,
-    //     vue: false,
-    //     posts: {
-    //         mainImage: '/images/shillaLogo.gif',
-    //         info: [
-    //             {
-    //                 pc: '/images/shillaDesk.png',
-    //                 alt: '데스크탑 정보',
-    //             },
-    //             {
-    //                 mobile: '/images/shillaMobile.png',
-    //                 alt: '모바일 정보',
-    //             },
-    //         ],
-    //         subtitle: [
-    //             '피그마와 HTML & CSS & JS를 사용하여',
-    //             '신라호텔 클론사이트를 마크업 하였습니다.',
-    //         ],
-    //         sites: [
-    //             {
-    //                 link: 'https://lothlorien91.cafe24.com/shilla/index.html',
-    //                 name: '클론 사이트',
-    //             },
-    //             {
-    //                 link: 'https://www.figma.com/file/VfxhccQfbG2aVegcaS7Yxt/Shilla?node-id=0%3A1&t=Ml5aJL2ch4NlSQgB-1',
-    //                 name: '피그마 상세정보',
-    //             },
-    //         ],
-    //         images: [
-    //             {
-    //                 pc: '/images/shillaPc.png',
-    //                 alt: '데스크탑 이미지',
-    //             },
-    //             {
-    //                 mobile: '/images/hackerRmo.png',
-    //                 alt: '모바일 이미지',
-    //             },
-    //         ],
-    //     },
-    // },
+    {
+        id: 2,
+        title: '포트폴리오',
+        vue: true,
+        markup: true,
+        posts: {
+            mainImage: '/images/logo.png',
+            description: [
+                'Vue.js와 Pinia & VueRouter를 사용하여',
+                '포트폴리오 사이트를 제작 하였습니다.',
+            ],
+            skill: ['html', 'scss', 'ts', 'vue'],
+            subInfo: [
+                {
+                    title: '반응형',
+                    content: 'O',
+                },
+                {
+                    title: '배포',
+                    content: 'Netlify',
+                },
+                {
+                    title: '라이브러리',
+                    content: 'fullPageJS',
+                },
+                {
+                    title: '총페이지',
+                    content: '메인 (1)',
+                },
+            ],
+            sites: [
+                {
+                    link: 'https://whitetree.netlify.app',
+                    name: '클론 사이트',
+                },
+                {
+                    link: 'https://www.figma.com/file/oH7FG7EQBigCOKb9iUfaCj/Portfolio?type=design&node-id=1%3A1155&t=H5Bg8NjwvEYBnWlC-1',
+                    name: '피그마 상세정보',
+                },
+            ],
+            images: [
+                {
+                    pc: '/images/portPc.jpg',
+                    alt: '데스크탑 이미지',
+                },
+            ],
+        },
+    },
+    {
+        id: 5,
+        title: '신라호텔',
+        markup: true,
+        react: false,
+        vue: false,
+        posts: {
+            mainImage: '/images/shillaLogo.gif',
+            subInfo: [
+                {
+                    title: '반응형',
+                    content: 'O',
+                },
+                {
+                    title: '배포',
+                    content: '카페24',
+                },
+                {
+                    title: '라이브러리',
+                    content: 'Slick',
+                },
+                {
+                    title: '총페이지',
+                    content: '메인 (1)',
+                },
+            ],
+            skill: ['html', 'css', 'js', 'scss'],
+            description: [
+                'HTML & CSS & JS를 사용하여 신라호텔',
+                '메인 페이지를 퍼블리싱 하였습니다.',
+            ],
+            sites: [
+                {
+                    link: 'https://lothlorien91.cafe24.com/shilla/index.html',
+                    name: '클론 사이트',
+                },
+                {
+                    link: 'https://www.figma.com/file/VfxhccQfbG2aVegcaS7Yxt/Shilla?type=design&node-id=0%3A1&t=D37Klze04rBWtPlX-1',
+                    name: '피그마 상세정보',
+                },
+            ],
+            images: [
+                {
+                    pc: '/images/shillaPc.png',
+                    alt: '데스크탑 이미지',
+                },
+            ],
+        },
+    },
     {
         id: 3,
         title: '해커뉴스',
